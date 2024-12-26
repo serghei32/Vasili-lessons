0d10dee90d112de82b56ea9155663caef03db308
serghei32 <115115828+serghei32@users.noreply.github.com>
2024-12-26 21:31:18 +0200

    first commidft

---
+++ script.js
@@ -0,0 +1,122 @@
+ // ------------------1 task
+
+
+ // for(let i=1; i<=10;i++){
+ //     console.log(i);
+
+ // }
+
+
+ //--------------2 task 
+
+ // const numbers=[1,2,3,4,5];
+
+ // for(let i=0; i<=numbers.length;i++){
+ //     console.log(numbers[i]);
+
+ // }
+
+
+ //--------------2 task  - help off chat gpt также путаюсь с извлечением и работой над индексами и значениями (масива)
+
+ //  const numbers = [1, 2, 3, 4, 5];
+ //  const result = [];
+ //  for (let i = 0; i < numbers.length; i++) {
+ //      if (numbers[i] >= 2 && numbers[i] <= 4) {
+ //          result.push(numbers[i])
+ //      } 
+ //  }
+ //  console.log(result);
+
+
+ //-----------------------3 task   почти сам 
+
+ //  const carsArray = ['mercedes', 'volvo', 'volkswagen', 'skoda'];
+ //  const cars = document.querySelector('.cars')  // достал div from  html 
+
+ //  const arrayToString= carsArray.toString()//  преобразовываю масив в список при помощи метода toString потому как createTextNode ожидает строку 
+
+ //  const carsNode= document.createTextNode(arrayToString) // добавляю этот список в текстовый узел путем создания переменной 
+
+ //  cars.appendChild(carsNode) // засовываю текстовый узел в div с помощью appendChild!
+ //  console.log(cars);
+
+
+ //-----------------------4 task 
+
+ //  const num= [2, 3, 4, 5] 
+ //  const res=[]
+ //  for(let i=0; i<= num.length;i++){ // бежим по масиву пока его длинна(индексов) это позволяет.включаю инкримент с шагом 1 
+ //     res.push(num[i]*5)// в пустой масив закидывю то что вывел мне индекс с умн на 5. 
+ //  }
+ //  console.log(res);
+
+
+ //-----------------------5 task  не получилось 
+ //  const week = ["пн", "вт", "ср", "чт", "пт", "сб", "вс"]
+
+ //  const weekNode = document.querySelector('.week') //достал див 
+
+ //  const textWeek = week.toString() //  преобразовал масив в текст
+ //  const textItemNode = document.createTextNode(textWeek) // создание текстового узла и помещение в него списка для дальнейшего его внедрения в dom element
+ //  const lastTwoElements = week.slice(-2) // создаю последние дня недели 
+ //  const spanNode6 = document.createElement('span') // создаю спан 
+ //  const spanNode6Text =document.createTextNode(lastTwoElements[0])
+ // //  spanNode6Text.style.color = 'red';
+ //  spanNode6.style.color = 'red';
+
+ // spanNode6.appendChild(spanNode6Text)
+
+ //  weekNode.appendChild(textItemNode)
+
+ // -------- 5 task ---
+
+
+//   const week = ["пн", "вт", "ср", "чт", "пт", "сб", "вс"]
+//   const weekNode = document.querySelector('.week')
+//   const ulNode = document.createElement('ul')
+//   weekNode.appendChild(ulNode)
+
+//   for (let i = 0; i < week.length; i++) { // счетчик будет сравниваться с индексами масива и шагать вперед 
+//       const liNode = document.createElement('li')// sozdal ellement
+//       liNode.textContent = week[i] // pomestil v etot element tekstovii uzel . тут в i будет отображаться значение счетчика на каждой итерации а не значение индекса. 
+//       ulNode.appendChild(liNode) // zasunul li v ul
+//       console.log(i);
+
+//       if (i === 5 || i === 6) { // значение счетчика сравниваем с значениями сб и вс которые имеют индекс  5 и 6 
+//           liNode.style.fontWeight= 'bold' // пишем nodeЭллементу стилистику
+//       }
+//   }
+
+ //---------------------------------------------
+
+
+//  const numberss = [1, 2, 4, -1, -3, 8, 7, 0, -5, 18, 12]
+// const resultat=[]
+//  for(let i =0; i<numberss.length; i++){
+//     if(numberss[i]>0 && numberss[i] <10 ) // сравниваю значение масива которое итерируется 
+//         resultat.push(numberss[i]) // вытаскиваю значение масива а не индекс   
+// }
+// document.write(resultat)
+
+
+// ----------------------------forEach
+
+const students = ['John', 'Sara', 'Jack'];
+
+students.forEach((item)=>{
+    if(item  )
+    console.log(`hi ${item}`);
+})
+
+
+const arrayItems = ['item1', 'item2', 'item3'];
+const copyItems = [];
+
+arrayItems.forEach((item)=>{
+    copyItems.push(item)
+})
+console.log(copyItems);
+
+
+
