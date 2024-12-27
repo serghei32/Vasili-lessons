c9f3510cf1f20253a7097fc873b68c4a2addaaed
serghei32 <115115828+serghei32@users.noreply.github.com>
2024-12-27 13:55:04 +0200

    second commit

--- script.js
+++ script.js
@@ -72,56 +72,110 @@
  // -------- 5 task ---
 
 
-//   const week = ["пн", "вт", "ср", "чт", "пт", "сб", "вс"]
-//   const weekNode = document.querySelector('.week')
-//   const ulNode = document.createElement('ul')
-//   weekNode.appendChild(ulNode)
+ //   const week = ["пн", "вт", "ср", "чт", "пт", "сб", "вс"]
+ //   const weekNode = document.querySelector('.week')
+ //   const ulNode = document.createElement('ul')
+ //   weekNode.appendChild(ulNode)
 
-//   for (let i = 0; i < week.length; i++) { // счетчик будет сравниваться с индексами масива и шагать вперед 
-//       const liNode = document.createElement('li')// sozdal ellement
-//       liNode.textContent = week[i] // pomestil v etot element tekstovii uzel . тут в i будет отображаться значение счетчика на каждой итерации а не значение индекса. 
-//       ulNode.appendChild(liNode) // zasunul li v ul
-//       console.log(i);
+ //   for (let i = 0; i < week.length; i++) { // счетчик будет сравниваться с индексами масива и шагать вперед 
+ //       const liNode = document.createElement('li')// sozdal ellement
+ //       liNode.textContent = week[i] // pomestil v etot element tekstovii uzel . тут в i будет отображаться значение счетчика на каждой итерации а не значение индекса. 
+ //       ulNode.appendChild(liNode) // zasunul li v ul
+ //       console.log(i);
 
-//       if (i === 5 || i === 6) { // значение счетчика сравниваем с значениями сб и вс которые имеют индекс  5 и 6 
-//           liNode.style.fontWeight= 'bold' // пишем nodeЭллементу стилистику
-//       }
-//   }
+ //       if (i === 5 || i === 6) { // значение счетчика сравниваем с значениями сб и вс которые имеют индекс  5 и 6 
+ //           liNode.style.fontWeight= 'bold' // пишем nodeЭллементу стилистику
+ //       }
+ //   }
 
  //---------------------------------------------
 
 
-//  const numberss = [1, 2, 4, -1, -3, 8, 7, 0, -5, 18, 12]
-// const resultat=[]
-//  for(let i =0; i<numberss.length; i++){
-//     if(numberss[i]>0 && numberss[i] <10 ) // сравниваю значение масива которое итерируется 
-//         resultat.push(numberss[i]) // вытаскиваю значение масива а не индекс   
-// }
-// document.write(resultat)
+ //  const numberss = [1, 2, 4, -1, -3, 8, 7, 0, -5, 18, 12]
+ // const resultat=[]
+ //  for(let i =0; i<numberss.length; i++){
+ //     if(numberss[i]>0 && numberss[i] <10 ) // сравниваю значение масива которое итерируется 
+ //         resultat.push(numberss[i]) // вытаскиваю значение масива а не индекс   
+ // }
+ // document.write(resultat)
 
 
-// ----------------------------forEach 1
+ // ----------------------------forEach 1
 
-const students = ['John', 'Sara', 'Jack'];
+ const students = ['John', 'Sara', 'Jack'];
 
-students.forEach((item)=>{
-    if(item  )
-    console.log(`hi ${item}`);
-})
+ students.forEach((item) => {
+     if (item)
+         console.log(`hi ${item}`);
+ })
 
-//------------------------- forEach 2
-const arrayItems = ['item1', 'item2', 'item3'];
-const copyItems = [];
+ //------------------------- forEach 2
+ const arrayItems = ['item1', 'item2', 'item3'];
+ const copyItems = [];
 
-arrayItems.forEach((item)=>{
-    copyItems.push(item)
-})
-console.log(copyItems);
+ arrayItems.forEach((item) => {
+     copyItems.push(item)
+ })
+ console.log(copyItems);
 
-//------------------------- forEach 2
-const scores = [5, 8, 3, 10, 7];
-newArray=[]
-scores.forEach((item)=>{
-    newArray.push(3*item)  
-})
-console.log(newArray);
+ //------------------------- forEach 3
+ const scores = [5, 8, 3, 10, 7];
+ newArray = []
+ scores.forEach((item) => {
+     newArray.push(3 * item)
+ })
+ console.log(newArray);
+
+ let res = scores.includes(9)
+ console.log(res);
+ //*********************************************************************
+
+ const courses = ['Biology', 'Mathematics', 'Chemistry', 'Physics'];
+
+ courses.forEach((item, index) => {
+     console.log(`${index}: ${item}`);
+ })
+
+ const grade = [33, 90, 78, 94, 56, 88, 97];
+ result = []
+ grade.forEach((item) => {
+     if (item > 70) {
+         result.push(item)
+
+     }
+ })
+ console.log(result);
+ //********************************************************************* 
+
+ //  const numbers = [1, 2, 3, 4, 5];
+ //  sum = 0
+ //  numbers.forEach((item) => {
+ //      sum = sum + item
+ //     })
+ //     console.log(sum);
+
+
+ //Умножение каждого элемента
+
+ const numbers = [10, 20, 30];
+ const summery = []
+ numbers.forEach((item) => {
+     let result = item * 2
+     summery.push(result)
+ })
+ console.log(summery);
+
+
+ //Напишите код, который создаёт массив из длин строк, используя forEach.
+ const words = ['apple', 'banana', 'cherry'];
+ const length = []
+ words.forEach((item, index) => {
+     length.push(item.length)
+     length.push(index)
+     
+ })
+
+ console.log(length);
+
+
+ 
